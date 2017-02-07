7666bcb833e9a293c209cb3178e17c4a61035348
malaruik <markus.alaruikka@elisanet.fi>
2017-02-07 05:41:49 -0800

    cf-agent mingw changes

---
+++ libutils/mingw_stubs.c
@@ -0,0 +1,32 @@
+#include <logging.h>
+#include <dir.h>
+
+Dir *DirOpen(const char *dirname)
+{
+  //MAla
+}
+
+const struct dirent *DirRead(Dir *dir)
+{
+  //MAla
+}
+
+void DirClose(Dir *dir)
+{
+  //MAla
+}
+
+int rpl_rename(const char *oldpath, const char *newpath)
+{
+  //MAla
+}
+
+int rpl_stat(const char *path, struct stat *buf)
+{
+  //MAla
+}
+
+int lstat(const char *file_name, struct stat *buf)
+{
+  //MAla
+}
