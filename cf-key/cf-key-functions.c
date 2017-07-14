9be92da584e49a17941c6543aefda6caa2f05c55
MarkusAlaruikka <markus.alaruikka@elisanet.fi>
2017-07-14 08:34:07 +0300

    make4

--- cf-key/cf-key-functions.c
+++ cf-key/cf-key-functions.c
@@ -27,7 +27,7 @@
 
 #include <openssl/bn.h>                                     /* BN_*, BIGNUM */
 #include <openssl/rand.h>                                   /* RAND_* */
-#include <rsa.h>
+#include <openssl/rsa.h>
 
 #include <lastseen.h>
 #include <dir.h>
