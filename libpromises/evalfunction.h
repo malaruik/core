21bb2bcd3ca2a976b07367899724e0ef7858ac74
MarkusAlaruikka <markus.alaruikka@elisanet.fi>
2017-07-16 22:23:25 +0300

    more stubs ..

--- libpromises/evalfunction.h
+++ libpromises/evalfunction.h
@@ -30,6 +30,17 @@
 #include <set.h>
 #include <fncall.h>
 
+// MAla stubs
+#define GetRegistryValue GetRegistryValue__stub
+#define CfLDAPList CfLDAPList__stub
+#define CfLDAPArray CfLDAPArray__stub
+#define CfRegLDAP CfRegLDAP__stub
+#define CacheUnreliableValue CacheUnreliableValue__stub
+#define RetrieveUnreliableValue RetrieveUnreliableValue__stub
+#define GetRemoteScalar GetRemoteScalar__stub
+#define CfLDAPValue CfLDAPValue__stub
+#define ListHostsWithClass ListHostsWithClass__stub
+
 FnCallResult FnCallHostInNetgroup(EvalContext *ctx, const Policy *policy, const FnCall *fp, const Rlist *finalargs);
 
 int FnNumArgs(const FnCallType *call_type);
