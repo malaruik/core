17d83ca2aa3b2ac99c65c9d4b42cdd741421b605
MarkusAlaruikka <markus.alaruikka@elisanet.fi>
2017-07-18 22:55:52 +0300

    fixs

---
+++ cf-agent/cf-agent-stubs.c
@@ -0,0 +1,25 @@
+include <cf3.defs.h>
+
+#include <cfnet.h>
+#include <comparray.h>
+#include <generic_agent.h>
+
+int GetOwnerName(char *path, struct stat *lstatptr, char *owner, int ownerSz)
+{
+  //MAla
+}
+
+PromiseResult VerifyRegistryPromise(EvalContext *ctx, Attributes a, const Promise *pp)
+{
+  //MAla
+}
+
+PromiseResult VerifyWindowsService(EvalContext *ctx, Attributes a, Promise *pp)
+{
+  //MAla
+}
+
+PromiseResult Nova_CheckNtACL(EvalContext *ctx, const char *file_path, Acl acl, Attributes a, const Promise *pp)
+{
+  //MAla
+}
