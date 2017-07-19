eef880d78fda89af776e4fb0683962fe4822fdbd
MarkusAlaruikka <markus.alaruikka@elisanet.fi>
2017-07-19 21:04:44 +0300

    fixsss

--- cf-agent/cf-agent-enterprise-stubs.h
+++ cf-agent/cf-agent-enterprise-stubs.h
@@ -33,15 +33,8 @@
 
 #if defined(__MINGW32__)
 
-#define Nova_Initialize Nova_Initialize__stub
-#define Nova_TrackExecution Nova_TrackExecution__stub
-#define GenerateReports GenerateReports__stub
-#define AgentDiagnosticsRunAllChecksNova AgentDiagnosticsRunAllChecksNova__stub
-#define Nova_NoteAgentExecutionPerformance Nova_NoteAgentExecutionPerformance__stub
-#define GenericAgentWriteVersion GenericAgentWriteVersion__stub
-
-
 PromiseResult VerifyRegistryPromise(EvalContext *ctx, Attributes a, const Promise *pp);
+
 #endif
 
 typedef bool (*CopyRegularFileFunction)(EvalContext *ctx,
