908d337abda222e00f16cf5b3cc0a24eba0f7dff
Markus Alaruikka <markus.alaruikka@op.fi>
2017-09-13 17:08:46 +0300

    log lines

--- libutils/mingw_stubs.c
+++ libutils/mingw_stubs.c
@@ -35,8 +35,6 @@ int rpl_stat(const char *path, struct stat *buf)
 {
   //MAla
 	Log(LOG_LEVEL_VERBOSE, "---- LIBUTILS stub: rpl_stat ..");
-	//int a = _stat(*path, *buf);
-	return _stat64(*path, struct stat *buf);
 }
 
 int lstat(const char *file_name, struct stat *buf)
