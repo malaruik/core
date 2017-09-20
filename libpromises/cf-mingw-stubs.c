e593f9bc626adbb666a61d998de6b7e3b568d396
Markus Alaruikka <markus.alaruikka@op.fi>
2017-09-20 22:09:15 +0300

    vsysname

--- libpromises/cf-mingw-stubs.c
+++ libpromises/cf-mingw-stubs.c
@@ -71,10 +71,10 @@ int NovaWin_GetEnv(char *varName, char *varContents, int varContentsSz)
 int NovaWin_FileExists(const char *fileName)
 {
 
-	 DWORD d = GetFileAttributes(fileName);
+	 DWORD dfile = GetFileAttributes(fileName);
 
-	 //Log(LOG_LEVEL_VERBOSE, "---- stubs: NovaWin_FileExists ..");
-	 //Log(LOG_LEVEL_VERBOSE, "---- return value: %s", GetFileAttributes(fileName));
+	 Log(LOG_LEVEL_VERBOSE, "---- stubs: NovaWin_FileExists ..");
+	 Log(LOG_LEVEL_VERBOSE, "---- return value: %d", dfile);
 
 	 return GetFileAttributes(fileName);
 }
