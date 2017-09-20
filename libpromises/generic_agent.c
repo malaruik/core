435a088c62c048fc6fba9a1c0f9c988a77eee696
Markus Alaruikka <markus.alaruikka@op.fi>
2017-09-20 10:06:00 +0300

    vsysname

--- libpromises/generic_agent.c
+++ libpromises/generic_agent.c
@@ -1451,7 +1451,7 @@ static void CheckWorkingDirectories(EvalContext *ctx)
     const char* const statedir = GetStateDir();
 
     //MAla uname replaced
-    if (GetComputerName(&VSYSNAME) == -1)
+    if (GetComputerName(&VSYSNAME, sizeof(VSYSNAME)) == -1)
     {
         Log(LOG_LEVEL_ERR, "Couldn't get kernel name info. (uname: %s)", GetErrorStr());
         memset(&VSYSNAME, 0, sizeof(VSYSNAME));
